259c5ccb7fb231d9976ea7f1d61477b0c1f36029
Libbi Mylah <95930674+libbi-mylah@users.noreply.github.com>
2022-12-07 15:41:29 -0500

    completed code for project

--- index.js
+++ index.js
@@ -1,20 +1,42 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const multer = require("multer");
+require("dotenv").config();
 
-var app = express();
+const app = express();
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.use((req, res, next) => {
+  console.log(
+    "method: " + req.method + "  |  path: " + req.path + "  |  IP - " + req.ip
+  );
+  next();
 });
 
+// ** SOLUTION **//
 
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
 
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  console.log(req.file);
+  if (req.file) {
+    res.json({
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size,
+    });
+  } else {
+    res.json("No file found");
+  }
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
